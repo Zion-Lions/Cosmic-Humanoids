e746a434d1b4d570d3bb898291057d9394afa4e2
Andréas Touly <andreas.touly@gmail.com>
2015-04-29 03:52:20 +0200

    Menu multijoueur (rien de foufou) + modification de l'HUD

--- Unity/Assets/Scripts/HUD.cs
+++ Unity/Assets/Scripts/HUD.cs
@@ -5,18 +5,25 @@ using System.Collections;
 public class HUD : MonoBehaviour
 {
     private GameObject MainCamera;
-    Text text;
+    public GameObject BulletsLeft;
+    public GameObject AllBulletsLeft;
+    private Text text1;
+    private Text text2;
     // Use this for initialization
     void Start()
     {
         MainCamera = GameObject.Find("Main Camera");
-        text = GetComponent<Text>();
-        text.fontSize = 25;
+        text1 = BulletsLeft.GetComponent<Text>();
+        text2 = AllBulletsLeft.GetComponent<Text>();
+        text1.fontSize = 50;
+        text1.fontStyle = FontStyle.Bold;
+        text2.fontSize = 30;
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = MainCamera.GetComponent<RayShoot>().BulletsLeft + " | " + MainCamera.GetComponent<RayShoot>().AllBulletsLeft;
+        text1.text = MainCamera.GetComponent<RayShoot>().BulletsLeft.ToString();
+        text2.text = MainCamera.GetComponent<RayShoot>().AllBulletsLeft.ToString();
     }
 }
