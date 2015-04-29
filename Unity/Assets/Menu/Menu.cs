a65b38d5e17ed47cc174fe9601a6945a32e7c479
mermaidXunicorn <farah.nedjadi@epita.fr>
2015-04-29 16:29:43 +0200

    Main Menu + correction beug "First Map"

---
+++ Unity/Assets/Menu/Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour 
+{
+    private Animator anim;
+    private CanvasGroup canGroup;
+
+    public bool IsOpen
+    {
+        get { return anim.GetBool("IsOpen"); }
+        set { anim.SetBool("IsOpen", value); }
+    }
+
+    public void Awake()
+    {
+        anim = GetComponent<Animator>();
+        canGroup = GetComponent<CanvasGroup>();
+
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+    }
+
+    public void Update()
+    {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+        {
+            canGroup.blocksRaycasts = canGroup.interactable = false;
+        }
+        else
+        {
+            canGroup.blocksRaycasts = canGroup.interactable = true;
+        }
+    }
+}
