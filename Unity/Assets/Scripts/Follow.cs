7fd0ff22488f3d5463e9a9d44e9497b25ace9ba0
beauthi <thibaut.passilly@epita.fr>
2015-06-17 19:44:10 +0200

    Push de salope
    André la pute

---
+++ Unity/Assets/Scripts/Follow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform Target;
+	public NavMeshAgent agent;
+	float f;
+	float rythme = 0f;
+	GUIText temp;
+	public GameObject[] Spawn;
+	Animator anim;
+	int t = 0;
+	bool opening = false;
+	bool closed = true;
+	GameObject Door;
+	float timer = 200f;  
+	bool playerInRange;
+	int attackDamage = 1;
+	float Dist;
+	float maxDist = 2f;
+	GameObject player;
+	Animator attack;
+	public bool activated;
+	Animation marche;
+
+
+	// Use this for initialization
+	void Start () {
+		agent = gameObject.GetComponent<NavMeshAgent>();
+		f = agent.speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		Target = player.transform;
+		agent.SetDestination(Target.position);
+	}
+}
---
+++ Unity/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	Transform target;
+	int moveSpeed = 3;
+	int rotationSpeed = 3;
+	Vector3 v;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		v.Set (target.position.x, transform.position.y, target.position.z);
+		transform.LookAt (v);	
+	}
+}
