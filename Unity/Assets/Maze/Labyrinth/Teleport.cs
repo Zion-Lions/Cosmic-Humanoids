8853a78fd66cf91f72878685051894a7a235ca8f
mermaidXunicorn <farah.nedjadi@epita.fr>
2015-06-18 13:13:58 +0200

    Modification de Game (map + teleporter)

---
+++ Unity/Assets/Maze/Labyrinth/Teleport.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour
+{
+    public int code;
+    float disableTimer = 0;
+
+    void Update()
+    {
+        if (disableTimer > 0)
+        {
+            disableTimer -= Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Perso complet" && disableTimer <= 0)
+        {
+                foreach (Teleport tp in FindObjectsOfType<Teleport>())
+                {
+                    if (tp.code == (code + 1))
+                    {
+                        tp.disableTimer = 2;
+                        Vector3 position = tp.gameObject.transform.position;
+                        position.y += 2;
+                        collider.gameObject.transform.position = position;
+                    }
+                }
+            
+        }
+    }
+}
