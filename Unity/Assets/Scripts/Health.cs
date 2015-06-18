1bfd5296947318bba86641e9c9984da3075ef668
beauthi <thibaut.passilly@epita.fr>
2015-06-19 00:45:01 +0200

    Spawn de l'ennemi (script - ne pas activer sinon Unity plante)

--- Unity/Assets/Scripts/Health.cs
+++ Unity/Assets/Scripts/Health.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Health : MonoBehaviour
 {
-
-    public int health;
-
+	public int health;
 	// Use this for initialization
 	void Start ()
-    {
-	
+    {	
+		Random r = new Random ();
+		if (gameObject.name == "Robot Kyle")
+						health = Random.Range (10, 150);
+				else
+						health = 100;
 	}
 	
 	// Update is called once per frame
---
+++ Unity/Assets/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public GameObject zombiePrefab; // prefab to spawn
+	int zombiesSpawned; // zombies spawned
+	GameObject player; // player
+	bool spawn;
+	int minWait;
+	int maxWait;
+	int waitTime;
+
+	// Use this for initialization
+	void Start () {
+		minWait = 1;
+		maxWait = 10;
+		waitTime = Random.Range (minWait, maxWait);
+		zombiesSpawned = 0;
+		spawn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spawn)
+						Spawn ();
+	}
+
+	void Spawn()
+	{
+		Instantiate (zombiePrefab, transform.position, transform.rotation); // spawn at spawner location
+		zombiesSpawned ++;
+		NewWaitTime ();
+		spawn = false;
+		SetSpawn ();
+	}
+
+	IEnumerator SetSpawn()
+	{
+		spawn = true;
+		yield return new WaitForSeconds(waitTime);
+	}
+
+	void NewWaitTime()
+	{
+		waitTime = Random.Range (minWait, maxWait);
+	}
+}
