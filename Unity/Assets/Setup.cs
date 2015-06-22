cd259b3054be3b71c3598e21ec79c00a72e8ea18
zionlion67 <bitzthomas@yahoo.fr>
2015-06-22 17:00:20 +0200

    new script #mission

--- Unity/Assets/Setup.cs
+++ Unity/Assets/Setup.cs
@@ -6,25 +6,52 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class Setup : NetworkBehaviour {
 
-    public RigidbodyFirstPersonController script;
-	void Start () {
-	
-             
-        if(!isLocalPlayer)
-        {
-          
-           
-            //GameObject.Find("HUDCanvas").SetActive(false);
-             GetComponent<RigidbodyFirstPersonController>().enabled = false;
-            
+    public float PositionLerpStep = 15f;
+    public float RotationLerpStep = 15f;
+    public GameObject camera;
+    [SyncVar]
+    private Vector3 syncPosition;
+    [SyncVar]
+    private Quaternion syncRotation;
 
-            
-            Debug.Log("OK");
-           
+    private Rigidbody rigidbody;
+
+	void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+        if(isLocalPlayer)
+        {
+            camera.SetActive(true);
         }
 	}
-	
-	
-	
-	
+
+    
+    void FixedUpdate()
+    {
+        if(isLocalPlayer)
+        {
+            CmdSendData(this.rigidbody.position, this.rigidbody.rotation);
+        }
+        if(!isServer && !isLocalPlayer)
+        {
+            Interpolate();
+        }
+    }
+
+    private void Interpolate()
+    {
+        Vector3 newPos = Vector3.Lerp(this.rigidbody.position, syncPosition, Time.deltaTime * PositionLerpStep);
+        rigidbody.MovePosition(newPos);
+
+        Quaternion newRot = Quaternion.Lerp(this.rigidbody.rotation, syncRotation, Time.deltaTime * RotationLerpStep);
+        rigidbody.MoveRotation(newRot);
+
+    } 
+
+    [Command]
+    private void CmdSendData(Vector3 pos, Quaternion rot)
+    {
+        syncPosition = pos;
+        syncRotation = rot;
+    }
 }
